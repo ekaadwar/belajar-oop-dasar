b25b6cd4f593fdee77f507cbfcb0c4387a1d98d6
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-03-14 18:12:15 +0700

    menerapkan prinsip overriding

---
+++ overriding.php
@@ -0,0 +1,60 @@
+<?php 
+	
+	class Produk{
+		public 	$judul = "judul",
+				$penulis = "penulis",
+				$penerbit = "penerbit",
+				$harga = 0;
+
+		public function __construct($judul, $penulis, $penerbit, $harga){
+			$this->judul = $judul;
+			$this->penulis = $penulis;
+			$this->penerbit = $penerbit;
+			$this->harga = $harga;
+		}
+
+		public function getLabel(){
+			return "$this->penulis, $this->penerbit";
+		}
+
+		public function getInfoLengkap(){
+			$str = "{$this->judul} | {$this->getLabel()} (Rp {$this->harga}) ";
+			return $str;
+		}
+	}
+
+	class Komik extends Produk {
+		public $halaman;
+
+		public function __construct($judul, $penulis, $penerbit, $harga, $halaman){
+			parent::__construct($judul, $penulis, $penerbit, $harga);
+			$this->halaman = $halaman;
+		}
+
+		public function getInfoLengkap(){
+			$str = "Komik : ". parent::getInfoLengkap() ." - {$this->halaman} Halaman.";
+			return $str;
+		}
+	}
+
+	class Game extends Produk{
+		public $waktuMain;
+
+		public function __construct($judul, $penulis, $penerbit, $harga, $waktuMain){
+			parent::__construct($judul, $penulis, $penerbit, $harga);
+			$this->$waktuMain = $waktuMain;
+		}
+
+		public function getInfoLengkap(){
+			$str = "Games : ". parent::getInfoLengkap() ." - {$this->waktuMain} Jam.";
+			return $str;
+		}
+	}
+
+	$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+	$produk2 = new Game("Resident Evil 6", "Sinji Mikami", "Capcom", 270000, 0, 70);
+	
+	echo "Dengan Overriding : <br>";
+	echo $produk1->getInfoLengkap();
+	echo "<br>{$produk2->getInfoLengkap()}";
+ ?>
