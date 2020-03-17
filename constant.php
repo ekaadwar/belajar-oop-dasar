55357c10094f9c8b7bd366ac2d1ad679a6431d89
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-03-17 16:56:47 +0700

    constant.php - belajar constant

---
+++ constant.php
@@ -0,0 +1,27 @@
+<?php 
+	// define("NAMA", "Eka Fajhari Adwar");
+	// echo NAMA;
+
+	// echo "<br>";
+
+	// const UMUR = 23;
+	// echo UMUR;
+
+	// class coba{
+	// 	//define ("NAMA", "Eka Fajhari Adwar"); ---> error
+	// 	const NAMA = "Eka Fajhari Adwar";
+	// }
+
+	// echo coba::NAMA;
+
+	
+
+	// echo __LINE__;
+	// echo "<br>";
+	// echo __FILE__;
+	// echo "<br>";
+	// echo __DIR__;
+
+	
+
+ ?>
