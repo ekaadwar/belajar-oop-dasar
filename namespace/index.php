3a4c183c3e7cf4d1220728f100a648b07019ea28
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-03-21 22:41:32 +0700

    belajar namespace

--- namespace/index.php
+++ namespace/index.php
@@ -2,10 +2,17 @@
 
 require "App/init.php";
 
-$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
-$produk2 = new Game("Resident Evil 6", "Sinji Mikami", "Capcom", 270000, 70); 		
+// $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+// $produk2 = new Game("Resident Evil 6", "Sinji Mikami", "Capcom", 270000, 70); 		
 
-$cetakProduk = new CetakInfoProduk();
-$cetakProduk->tambahProduk($produk1);
-$cetakProduk->tambahProduk($produk2);
-echo $cetakProduk->cetak();
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk($produk1);
+// $cetakProduk->tambahProduk($produk2);
+// echo $cetakProduk->cetak();
+
+use App\Produk\User as ProdukUser;
+use App\Service\User as ServiceUser;
+
+new ProdukUser();
+echo "<br>";
+new ServiceUser();
